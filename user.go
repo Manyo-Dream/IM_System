3f23ec1d05f875126c9901fe71dec28cd4b615ab
Manyo-Dream <manyodream@foxmail.com>
2025-07-14 17:58:03 +0800

    查询在线用户

--- user.go
+++ user.go
@@ -14,10 +14,10 @@ type User struct {
 func NewUser(conn net.Conn, server *Server) *User {
 	userAddr := conn.RemoteAddr().String()
 	user := &User{
-		Name: userAddr,
-		Addr: userAddr,
-		C:    make(chan string),
-		conn: conn,
+		Name:   userAddr,
+		Addr:   userAddr,
+		C:      make(chan string),
+		conn:   conn,
 		server: server,
 	}
 
@@ -26,7 +26,7 @@ func NewUser(conn net.Conn, server *Server) *User {
 	return user
 }
 
-//用户上线功能
+// 用户上线功能
 func (u *User) Online() {
 	u.server.mapLock.Lock()
 	u.server.OnlineMap[u.Name] = u
@@ -35,7 +35,7 @@ func (u *User) Online() {
 	u.server.BroadCast(u, "已上线")
 }
 
-//用户下线功能
+// 用户下线功能
 func (u *User) Offline() {
 	u.server.mapLock.Lock()
 	delete(u.server.OnlineMap, u.Name)
@@ -44,11 +44,21 @@ func (u *User) Offline() {
 	u.server.BroadCast(u, "已下线")
 }
 
-//处理用户消息
-func (u *User) DoMessage(msg string) {
-	u.server.BroadCast(u, msg)
+func (u *User) SendMsg(msg string) {
+	u.conn.Write([]byte(msg))
 }
 
+// 处理用户消息
+func (u *User) DoMessage(msg string) {
+	if msg == "who" {
+		for _, user := range u.server.OnlineMap {
+			onlineMsg := "[" + user.Addr + "]" + user.Name + "\n"
+			u.SendMsg(onlineMsg)
+		}
+	} else {
+		u.server.BroadCast(u, msg)
+	}
+}
 
 // 监听当前User Channel的方法，一旦有消息，就发送给客户端
 func (u *User) ListenMessage() {
